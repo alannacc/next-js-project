08213763698a4685dade85c26b0579b32ed5970e
Alanna Clarke <alannacalendar@gmail.com>
2022-09-30 10:37:16 -0600

    first commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Meta from '../components/Meta'
+
+const about = () => {
+    return (
+        <div>
+            <Meta title='About'/>
+            <Head>
+                <title>About</title>
+            </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default about
